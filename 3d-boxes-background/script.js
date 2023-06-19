133f4c715740c32875787faa7fc6bd614ab96b1a
Diana Liu <mata7890@sina.com>
2023-06-19 16:43:18 +1000

    3D boxes background: break bg img to pieces when twirling

--- 3d-boxes-background/script.js
+++ 3d-boxes-background/script.js
@@ -1,17 +1,23 @@
 const boxesContainer = document.getElementById('boxes')
 const btn = document.getElementById('btn')
 
-btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+btn.addEventListener('click', () => {
+  console.log("before event listener");
+  boxesContainer.classList.toggle('big');
+  
+  console.log("after event listener");})
+  
 
 function createBoxes() {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       const box = document.createElement('div')
-      box.classList.add('box')
+      box.classList.add('box');
       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
       boxesContainer.appendChild(box)
     }
   }
 }
-
+console.log("before calling createbox()");
 createBoxes()
+console.log("after calling createbox");
