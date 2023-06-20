03bed88c223503282a50901319822be04a581e9c
Diana Liu <mata7890@sina.com>
2023-06-20 15:55:13 +1000

    animated-countdown: 1 bug fixed. used two timer, one for removing rotating animation, the other one for adding rotating animation.

--- animated-countdown/1.js
+++ animated-countdown/1.js
@@ -1,30 +1,36 @@
 const div_count = document.getElementById('count');
 const div_countDownContainer = document.getElementById('countDownContainer');
 const div_goContainer = document.getElementById('goContainer')
-const p_getready = document.querySelector('#countDownContainer p');
-const p_go = document.querySelector('#goContainer p');
 const btn = document.getElementsByTagName('button')[0];
 
 const myfunction = () => {
-    let count = 3;
+    let count = 5;
     div_count.textContent = count;
     div_count.classList.add('in');
-    div_goContainer.classList.add('in');
-    div_countDownContainer.classList.add('out');
     div_goContainer.hidden = true;
-    div_countDownContainer.hidden=false;
-    console.log(count);
+    div_goContainer.classList.remove('in');
+    div_countDownContainer.classList.remove('out');
+
+    let c = 0;//async timer to remove counter animation
+    setTimeout(() => {
+        c += 1;
+        div_count.classList.remove('in');
+        const myInterval2 = setInterval(() => {
+            div_count.classList.remove('in');
+            if (c == count + 1) { clearInterval(myInterval2) }
+        }, 1000)
+    }, 900);
+
     const myInterval = setInterval(() => {
         count -= 1;
         if (count < 0) {
-            div_count.classList.remove('in');
+            div_countDownContainer.classList.add('out');
             div_goContainer.hidden = false;
-            if (count < -1) {
-                clearInterval(myInterval);
-                div_countDownContainer.hidden=true;
-            }
+            div_goContainer.classList.add('in');
+            clearInterval(myInterval);
         } else {
             div_count.textContent = count;
+            div_count.classList.add('in');
         }
     }, 1000);
 }
